fbe3f30be5d313ba4d2571822d5c3e8774c520f4
lizhiyuan <148334733@qq.com>
2018-11-19 19:14:14 +0800

    删除了一些不必要的文件

--- day1/write.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <stdlib.h>
-int main(){
-    //写入文件
-    if((write(1,"here is some data\n",18))!=18){
-        write(2,"a write error has occurend on file descriptor 1\n",46);
-    }
-    exit(0);
-}
