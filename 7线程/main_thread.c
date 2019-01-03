319ac5f911cf3c24b8114ed63659b1ef2176ff09
lizhiyuan <148334733@qq.com>
2019-01-03 21:31:43 +0800

    线程的取消

--- 7线程/main_thread.c
+++ 7线程/main_thread.c
@@ -7,11 +7,33 @@
 struct student {
     int age;
     char name[20];
+    char id[4];
 };
-void *thrread_fun(void *stu){
-    printf("student age is %d,name is %s\n",((struct student *)stu)->age,((struct student *)stu)->name);
+void *thread_fun(void *stu){
+    sleep(1);
+    printf("student age is %d,name is %s,id is %s\n",((struct student *)stu)->age,((struct student *)stu)->name,((struct student *)stu)->id);
     return (void *)0;
 };
-int main(){
+int main(int argc,char *argv[]){
+    pthread_t tid;
+    int err;
+    int *rval;
+    struct student stu;
+    stu.age = 20;
+    memcpy(stu.name,"zhangsan",20);
+    memcpy(stu.id,"0007",4);
+    err = pthread_create(&tid,NULL,thread_fun,(void *)(&stu));
+    if(err != 0){
+        printf("create new thread failure\n");
+        return 0;
+    }
+    int i;
+    printf("main thread hava %d args\n",argc);
+    for(i=0;i<argc;i++){
+        printf("main thread args is %s\n",argv[i]);
+    }
 
+    //sleep(1);//主线程会导致所有的其他线程结束
+    //return 0;
+    pthread_exit(rval);//主线程会等待其他的线程结束后再结束
 }
